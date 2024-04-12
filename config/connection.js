c4ae842f0840420045e84bfd9258ea1a4323efc6
Jus Ferrell <j1ferrel@ucsd.edu>
2024-04-11 22:07:42 -0700

    minor revisions

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
-const mongoURI = process.env.MONGODB_URI;
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected...'))
-  .catch(err => console.error(err));
+console.log('MongoDB URI:', process.env.MONGODB_URI);
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 module.exports = mongoose.connection;
